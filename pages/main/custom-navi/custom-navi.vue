c45c98c675a16d154c3b25ef7a60a36ca58bf33f
objcat <objcat@163.com>
2020-06-19 10:10:40 +0800

    Revert "解决swiper中的第二张image会覆盖position控件的问题"
    This reverts commit fb1fe4b5d6058ef8a198feae6a08d39eacc450c1.

--- pages/main/custom-navi/custom-navi.vue
+++ pages/main/custom-navi/custom-navi.vue
@@ -1,12 +1,30 @@
 <template>
-	<view class="zy-content">
-		<u-navbar title="剑未配妥，出门已是江湖" :background="{'background': '#007AFF'}" backIconColor="#FFFFFF" titleColor="#FFFFFF" backText=" "></u-navbar>
+	<view class="zy-content" v-if="hidden">
+		
+		<view style="width: 100px; height: 100px; background-color: red;">
+			
+		</view>
+		<u-navbar back-text="返回" title="剑未配妥，出门已是江湖" :background="{'background': '#007AFF'}" backIconColor="#FFFFFF" titleColor="#FFFFFF" backText=""></u-navbar>
+		<view class="zy-flex-column">
+			
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
-		
+		data() {
+			return {
+				hidden: false
+			}
+		},
+		onLoad() {
+			let self = this;
+			setTimeout(function(){
+				self.hidden = true;
+			}, 50)
+			
+		}
 	}
 </script>
 
