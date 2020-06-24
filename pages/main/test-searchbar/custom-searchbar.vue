a626a9bc72b131b49cb78304252774b9cc368353
objcat <objcat@163.com>
2020-06-24 13:20:41 +0800

    完成了自定义搜索框相对应的案例

---
+++ pages/main/test-searchbar/custom-searchbar.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="zy-content">
+		<u-navbar back-text="返回" :background="{'background': '#007AFF'}" backIconColor="#FFFFFF" titleColor="#FFFFFF" backText=" ">
+			<view class="search-wrap">
+				<!-- 如果使用u-search组件，必须要给v-model绑定一个变量 -->
+				<u-search v-model="keyword" height="56" :action-style="{color: '#fff'}" @change="change" @search="search" @custom="custom"></u-search>
+			</view>
+		</u-navbar>
+	</view>
+</template>
+
+<script>
+	import {
+		req,
+		api,
+		navi,
+		img,
+		ui 
+	} from '@/js/zykit.js'
+	export default {
+		data() {
+			return {
+				keyword: ""
+			}
+		},
+		methods: {
+			// 文字改变
+			change: function(res) {
+				console.log(res);
+			},
+			// 点击键盘上的搜索
+			search: function(res) {
+				ui.showToast("搜索内容:" + res);
+				console.log(res);
+			},
+			// 点击搜索按钮
+			custom: function(res) {
+				console.log(res);
+			}
+		}
+	}
+</script>
+
+<style>
+	.search-wrap {
+		margin: 0 20rpx;
+		flex: 1;
+	}
+</style>
--- pages/main/test-searchbar/test-searchbar.vue
+++ pages/main/test-searchbar/test-searchbar.vue
@@ -1,32 +1,49 @@
 <template>
 	<view class="zy-content" style="padding: 10px;">
+
+		<u-button type="primary" @click="tap">自定义搜索框</u-button>
+
 		<text class="break-all">原生导航栏搜索框有两种搜索模式, 搜索框需要在pages.json中进行配置</text>
-		
+
 		<text class="break-all">一.当前页面搜索</text>
-		
+
 		<text class="break-all">
 			当前页面搜索不需要禁用导航栏的
 			"disabled": false
 		</text>
-		
+
 		<text class="break-all">二.跳转新页面搜索</text>
-		
+
 		<text class="break-all">
 			新页面搜索需要禁用导航栏的文字输入事件
 			"disabled": true
 		</text>
-		
-		<text>详见如下网址: https://uniapp.dcloud.io/collocation/frame/lifecycle</text>
-		
+
+		<text class="break-all">详见如下网址: https://uniapp.dcloud.io/collocation/frame/lifecycle</text>
 		<text style="font-size: 20px; margin-top: 10px;">搜索框中的文字</text>
-		
 		<text class="break-all" style="background-color: #18B566; margin-top: 10px; font-size: 18px;">{{ text }}</text>
-		
+
+
+
+
 	</view>
 </template>
 
 <script>
+	import {
+		req,
+		api,
+		navi,
+		img,
+		ui 
+	} from '@/js/zykit.js'
 	export default {
+		methods: {
+			tap() {
+				console.log(123);
+				navi.navigateTo("/pages/main/test-searchbar/custom-searchbar");
+			}
+		},
 		data() {
 			return {
 				text: ""
